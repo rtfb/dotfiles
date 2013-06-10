b1de033eebfd5f8d27c570b37c8cccd972b93956
Vytautas Šaltenis <Vytautas.Shaltenis@gmail.com>
2013-06-10 23:02:57 +0300

    Make downloads optional
    Add command line param -f, which will call all the downloadable stuff.
    Without it, the script will only make changes available locally.

--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 function usage {
-    echo "Usage: $0 [email | -d]"
+    echo "Usage: $0 [-f] [email | -d]"
+    echo -e "\t-f will enable full install, including downloads"
     echo -e "\t-d will use the default email, $default_email"
     exit
 }
@@ -15,10 +16,15 @@ user_name=`whoami`
 user_record=`getent passwd $user_name`
 user_gecos_field=`echo "$user_record" | cut -d ':' -f 5`
 user_full_name=`echo "$user_gecos_field" | cut -d ',' -f 1`
+full_install=0
 
 if [ $# -eq 0 ]; then
     usage
 else
+    if [ $1 == "-f" ]; then
+        full_install=1
+        shift
+    fi
     if [ $1 == "-d" ]; then
         user_email=$default_email
     else
@@ -31,19 +37,73 @@ fi
 #================
 # SourceCodePro font
 #================
-if ! [ -f ~/.fonts/SourceCodePro-Regular.ttf] ; then
-    wget -O /tmp/SourceCodePro.zip https://github.com/downloads/adobe/Source-Code-Pro/SourceCodePro_FontsOnly-1.010.zip
-    unzip -o -x /tmp/SourceCodePro.zip -d /tmp/SourceCodePro
-    mkdir -p ~/.fonts
-    chmod +w /tmp/SourceCodePro/*/TTF/*.ttf
-    cp /tmp/SourceCodePro/*/TTF/*.ttf ~/.fonts
-    fc-cache
+function install_font {
+    if ! [ -f ~/.fonts/SourceCodePro-Regular.ttf] ; then
+        wget -O /tmp/SourceCodePro.zip https://github.com/downloads/adobe/Source-Code-Pro/SourceCodePro_FontsOnly-1.010.zip
+        unzip -o -x /tmp/SourceCodePro.zip -d /tmp/SourceCodePro
+        mkdir -p ~/.fonts
+        chmod +w /tmp/SourceCodePro/*/TTF/*.ttf
+        cp /tmp/SourceCodePro/*/TTF/*.ttf ~/.fonts
+        fc-cache
+    fi
+}
+
+function install_vim_plugins {
+    # Handle plugins:
+    # Fugitive: http://www.vim.org/scripts/script.php?script_id=2975
+    wget -O /tmp/fugitive.zip http://www.vim.org/scripts/download_script.php?src_id=15542
+    unzip -o -x /tmp/fugitive.zip -d ~/.vim
+
+    # Unimpaired: http://www.vim.org/scripts/script.php?script_id=1590
+    wget -O /tmp/unimpaired.zip http://www.vim.org/scripts/download_script.php?src_id=12570
+    unzip -o -x /tmp/unimpaired.zip -d ~/.vim
+
+    # Bufexplorer: http://www.vim.org/scripts/script.php?script_id=42
+    wget -O /tmp/bufexplorer.zip http://www.vim.org/scripts/download_script.php?src_id=14208
+    unzip -o -x /tmp/bufexplorer.zip -d ~/.vim
+
+    # Matchit
+    wget -O /tmp/matchit.zip http://www.vim.org/scripts/download_script.php?src_id=8196
+    unzip -o -x /tmp/matchit.zip -d ~/.vim
+
+    # Supertab: http://www.vim.org/scripts/script.php?script_id=1643
+    wget -O /tmp/supertab.vmb http://www.vim.org/scripts/download_script.php?src_id=18075
+    vim -c 'so %' -c 'q' /tmp/supertab.vmb
+
+    # CtrlP: http://kien.github.com/ctrlp.vim/
+    ctrlp="$HOME/.vim/bundle/ctrlp.vim"
+
+    if [ -d $crlp ]; then
+        rm -rf $ctrlp
+    fi
+
+    git clone https://github.com/kien/ctrlp.vim.git $ctrlp
+    vim -c 'helptags ~/.vim/bundle/ctrlp.vim/doc' -c 'q'
+
+    # Go stuff: git://github.com/jnwhiteh/vim-golang.git
+    vim_golang=/tmp/vim-golang
+
+    if [ -d $vim_golang ]; then
+        rm -rf $vim_golang
+    fi
+
+    git clone git://github.com/jnwhiteh/vim-golang.git $vim_golang
+    rm -rf $vim_golang/.git
+    cp -r $vim_golang/* $HOME/.vim
+}
+
+if [ $full_install -eq 1 ]; then
+    install_font
+    install_vim_plugins
 fi
 
 #================
 # Vim
 #================
 ln -s $here/vimrc ~/.vimrc
+mkdir -p ~/.vim/ftdetect
+mkdir -p ~/.vim/ftplugin
+mkdir -p ~/.vim/syntax
 mkdir -p ~/.vim/spell
 
 # I should be doing this:
@@ -53,50 +113,7 @@ mkdir -p ~/.vim/spell
 # workaround:
 cp lt.utf-8.spl ~/.vim/spell/lt.utf-8.spl
 
-# Handle plugins:
-# Fugitive: http://www.vim.org/scripts/script.php?script_id=2975
-wget -O /tmp/fugitive.zip http://www.vim.org/scripts/download_script.php?src_id=15542
-unzip -o -x /tmp/fugitive.zip -d ~/.vim
-
-# Unimpaired: http://www.vim.org/scripts/script.php?script_id=1590
-wget -O /tmp/unimpaired.zip http://www.vim.org/scripts/download_script.php?src_id=12570
-unzip -o -x /tmp/unimpaired.zip -d ~/.vim
-
-# Bufexplorer: http://www.vim.org/scripts/script.php?script_id=42
-wget -O /tmp/bufexplorer.zip http://www.vim.org/scripts/download_script.php?src_id=14208
-unzip -o -x /tmp/bufexplorer.zip -d ~/.vim
-
-# Matchit
-wget -O /tmp/matchit.zip http://www.vim.org/scripts/download_script.php?src_id=8196
-unzip -o -x /tmp/matchit.zip -d ~/.vim
-
-# Supertab: http://www.vim.org/scripts/script.php?script_id=1643
-wget -O /tmp/supertab.vmb http://www.vim.org/scripts/download_script.php?src_id=18075
-vim -c 'so %' -c 'q' /tmp/supertab.vmb
-
-# CtrlP: http://kien.github.com/ctrlp.vim/
-ctrlp="$HOME/.vim/bundle/ctrlp.vim"
-
-if [ -d $crlp ]; then
-    rm -rf $ctrlp
-fi
-
-git clone https://github.com/kien/ctrlp.vim.git $ctrlp
-vim -c 'helptags ~/.vim/bundle/ctrlp.vim/doc' -c 'q'
-
-# Go stuff: git://github.com/jnwhiteh/vim-golang.git
-vim_golang=/tmp/vim-golang
-
-if [ -d $vim_golang ]; then
-    rm -rf $vim_golang
-fi
-
-git clone git://github.com/jnwhiteh/vim-golang.git $vim_golang
-rm -rf $vim_golang/.git
-cp -r $vim_golang/* $HOME/.vim
-
 # ftplugin for .po files
-mkdir -p ~/.vim/ftplugin
 ln -s $here/vim/ftplugin/po.vim ~/.vim/ftplugin
 
 # ftplugin and syntax for hg commits
