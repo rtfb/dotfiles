77c1ab3b9d88a2de85e63e2b14cf9da3518e20d0
Vytautas Šaltenis <vytas@rtfb.lt>
2014-12-19 18:31:56 +0200

    Add Vim to a list of packages, add --yes flag

--- apt-get-on-clean-box.sh
+++ apt-get-on-clean-box.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # regular software, esp. one that I have bindings for:
-apt-get install curl wget scrot gimp tree abcde flac
+apt-get install vim-gtk curl wget scrot gimp tree abcde flac -y
 
 # build deps for i3lock:
-apt-get build-dep i3lock
-apt-get install libxcb-xkb-dev libxkbcommon-x11-dev
+apt-get build-dep i3lock -y
+apt-get install libxcb-xkb-dev libxkbcommon-x11-dev -y
