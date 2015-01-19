fe7e93d48557c355c6f66e3d891600b84b5d084a
Vytautas Šaltenis <vytas@rtfb.lt>
2015-01-19 11:51:30 +0200

    ekto.py: split up the actual unzipping code
    Move the two 'if not dry_run' clauses together.

--- ekto.py
+++ ekto.py
@@ -5,6 +5,18 @@ import sys
 import zipfile
 
 
+def unzip_archive(archive, target_dir):
+    os.makedirs(target_dir)
+    z = zipfile.ZipFile(archive)
+    infolist = z.infolist()
+    for zinfo in infolist:
+        if zinfo.flag_bits & 0x800:
+            out_file_name = zinfo.filename.decode('utf-8')
+        else:
+            out_file_name = zinfo.filename.decode('cp437')
+        open(os.path.join(target_dir, out_file_name), 'w').write(z.read(zinfo))
+
+
 def main():
     dry_run = False
     input_file = sys.argv[1]
@@ -20,21 +32,14 @@ def main():
         unzip_path = os.path.join(parts[0], parts[2] + ' - ' + parts[1])
     mus = os.environ['MUSIC_DIR']
     path = os.path.join(mus, unzip_path)
-    if not dry_run:
-        os.makedirs(path)
     dry_prefix = ''
     if dry_run:
         dry_prefix = '[DRY] '
     print('%sUnzipping "%s" to "%s"...' % (dry_prefix, input_file, path))
     if not dry_run:
-        z = zipfile.ZipFile(input_file)
-        infolist = z.infolist()
-        for zinfo in infolist:
-            if zinfo.flag_bits & 0x800:
-                out_file_name = zinfo.filename.decode('utf-8')
-            else:
-                out_file_name = zinfo.filename.decode('cp437')
-            open(os.path.join(path, out_file_name), 'w').write(z.read(zinfo))
+    # TODO: don't extract it straight into MUSIC_DIR, extract it to temp
+    # instead and move to the final place.
+        unzip_archive(input_file, path)
 
 
 if __name__ == '__main__':
