ff24182aba6b1102a719b25cbe6904cb4a407121
Vytautas Šaltenis <vytas@rtfb.lt>
2015-08-18 20:25:56 +0300

    Remove i3lock leftover

--- apt-get-on-clean-box.sh
+++ apt-get-on-clean-box.sh
@@ -2,7 +2,3 @@
 
 # regular software, esp. one that I have bindings for:
 apt-get install vim-gtk curl wget scrot gimp tree abcde flac -y
-
-# build deps for i3lock:
-apt-get build-dep i3lock -y
-apt-get install libxcb-xkb-dev libxkbcommon-x11-dev -y
