3c55a3ce11338c3c584bb39353d1db70248c39ae
Vytautas Šaltenis <vytas@rtfb.lt>
2015-08-22 16:41:04 +0300

    Add some missing packages to apt-get

--- apt-get-on-clean-box.sh
+++ apt-get-on-clean-box.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 # regular software, esp. one that I have bindings for:
-apt-get install vim-gtk curl wget scrot gimp tree abcde flac -y
+apt-get install vim-gtk curl wget scrot gimp tree abcde flac xclip parcellite \
+        numlockx -y
