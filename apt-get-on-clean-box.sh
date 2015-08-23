41dedf7df803651d3d221e91579b2550895a069d
Vytautas Šaltenis <vytas@rtfb.lt>
2015-08-23 20:29:00 +0300

    Update Dropbox installation
    Install in home dir, yay.

--- apt-get-on-clean-box.sh
+++ apt-get-on-clean-box.sh
@@ -3,3 +3,5 @@
 # regular software, esp. one that I have bindings for:
 apt-get install vim-gtk curl wget scrot gimp tree abcde flac xclip parcellite \
         numlockx -y
+cd ~
+su rtfb -c "wget -O - \"https://www.dropbox.com/download?plat=lnx.x86_64\" | tar xzf -"
