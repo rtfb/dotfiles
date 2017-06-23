28e6c1c36455fb3a95d74e44debe855321908c3d
Vytautas Šaltenis <vytas@rtfb.lt>
2017-06-23 21:53:51 +0300

    Fix clean box installs

--- apt-get-on-clean-box.sh
+++ apt-get-on-clean-box.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
+apt-get update
+
 # regular software, esp. one that I have bindings for:
-apt-get install vim-gtk curl wget scrot gimp tree abcde flac xclip parcellite \
-        numlockx -y
+apt-get --yes install \
+    curl wget scrot tree abcde flac xclip parcellite numlockx fontconfig
+# GUI things:
+apt-get --yes install vim-gtk gimp
+# needed for prepare.sh:
+apt-get --yes install unzip
 # install jump:
-curl -o /tmp/jump_0.8.0_amd64.deb https://github.com/gsamokovarov/jump/releases/download/v0.8.0/jump_0.8.0_amd64.deb
-dpkg -i jump_0.8.0_amd64.deb
+wget -O /tmp/jump_0.8.0_amd64.deb https://github.com/gsamokovarov/jump/releases/download/v0.8.0/jump_0.8.0_amd64.deb
+dpkg -i /tmp/jump_0.8.0_amd64.deb
 cd ~
 su rtfb -c "wget -O - \"https://www.dropbox.com/download?plat=lnx.x86_64\" | tar xzf -"
