cfea5d231ad69335bf1b19f449ed5eedfbf86ace
Vytautas Saltenis <vytas@uber.com>
2018-06-18 15:52:30 +0300

    apt: add units

--- apt-get-on-clean-box.sh
+++ apt-get-on-clean-box.sh
@@ -4,11 +4,15 @@ apt-get update
 
 # regular software, esp. one that I have bindings for:
 apt-get --yes install \
-    curl wget scrot tree abcde flac xclip parcellite numlockx fontconfig
+    curl wget scrot tree abcde flac xclip parcellite numlockx fontconfig \
+    units
+
 # GUI things:
 apt-get --yes install vim-gtk gimp
+
 # needed for prepare.sh:
 apt-get --yes install unzip
+
 # install jump:
 wget -O /tmp/jump_0.8.0_amd64.deb https://github.com/gsamokovarov/jump/releases/download/v0.8.0/jump_0.8.0_amd64.deb
 dpkg -i /tmp/jump_0.8.0_amd64.deb
