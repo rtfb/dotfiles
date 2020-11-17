bbd4add2654d45929f042b8ed6490de85e85ed06
rtfb <vytas@rtfb.lt>
2020-11-17 17:54:08 +0200

    Bump the version of jump

--- apt-get-on-clean-box.sh
+++ apt-get-on-clean-box.sh
@@ -14,7 +14,7 @@ apt-get --yes install vim-gtk gimp
 apt-get --yes install unzip
 
 # install jump:
-wget -O /tmp/jump_0.8.0_amd64.deb https://github.com/gsamokovarov/jump/releases/download/v0.8.0/jump_0.8.0_amd64.deb
-dpkg -i /tmp/jump_0.8.0_amd64.deb
+wget -O /tmp/jump_0.30.1_amd64.deb https://github.com/gsamokovarov/jump/releases/download/v0.30.1/jump_0.30.1_amd64.deb
+dpkg -i /tmp/jump_0.30.1_amd64.deb
 cd ~
 su rtfb -c "wget -O - \"https://www.dropbox.com/download?plat=lnx.x86_64\" | tar xzf -"
