aa8c40cbaa4673af161a1b91064113fb25fc45f0
rtfb <vytas@rtfb.lt>
2020-11-18 11:00:51 +0200

    Convert the list of packages to an array
    So that I can comment on each item as needed.

--- apt-get-on-clean-box.sh
+++ apt-get-on-clean-box.sh
@@ -1,18 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 
 apt-get update
 
 # regular software, esp. one that I have bindings for:
-apt-get --yes install \
-    curl wget scrot tree abcde flac xclip parcellite numlockx fontconfig \
-    silversearcher-ag units \
+PKGS=(
+    curl
+    wget
+    scrot
+    tree
+    abcde
+    flac
+    xclip
+    parcellite
+    numlockx
+    fontconfig
+    silversearcher-ag
+    units
     libgnome2-bin # gnome-open
+    unzip # needed for prepare.sh
 
-# GUI things:
-apt-get --yes install vim-gtk gimp
+    # GUI things:
+    vim-gtk
+    gimp
+)
 
-# needed for prepare.sh:
-apt-get --yes install unzip
+apt-get --yes install ${PKGS[@]}
 
 # install jump:
 wget -O /tmp/jump_0.30.1_amd64.deb https://github.com/gsamokovarov/jump/releases/download/v0.30.1/jump_0.30.1_amd64.deb
