7363323f007241f9402ef44493983144466901aa
Vytautas Saltenis <vytas@uber.com>
2021-08-23 20:32:09 +0300

    Install glow: a terminal Markdown viewer

--- apt-get-on-clean-box.sh
+++ apt-get-on-clean-box.sh
@@ -36,6 +36,13 @@ then
     dpkg -i /tmp/jump_0.30.1_amd64.deb
 fi
 
+if ! command -v glow &> /dev/null
+then
+    echo "glow not found, installing..."
+    wget -O /tmp/glow_1.4.1_linux_amd64.deb https://github.com/charmbracelet/glow/releases/download/v1.4.1/glow_1.4.1_linux_amd64.deb
+    dpkg -i /tmp/glow_1.4.1_linux_amd64.deb
+fi
+
 if ! [ -d ~/.dropbox-dist ] && [ "$(hostname)" == "dungeon" ]; then
     echo "dropbox not found, installing..."
     cd ~
